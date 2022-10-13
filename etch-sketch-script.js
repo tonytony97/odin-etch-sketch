17b798a80ed84c7f295eba5ad79467fb3b333f39
tony <nintego92@gmail.com>
2022-10-12 19:19:14 -0500

    Add event listener to square divs

--- etch-sketch-script.js
+++ etch-sketch-script.js
@@ -1,21 +1,27 @@
-
-const container = document.querySelector('#container');
-
 function makeGrid(dimensions){
     const grid = document.createElement('div');
     grid.classList.add('grid');
     grid.style.display='grid';
     grid.style.border = 'solid black';
-    grid.style.background ='orange';
     grid.style.setProperty('--grid-rows',dimensions);
     grid.style.setProperty('--grid-cols',dimensions);
+
     for(i= 0;i<dimensions*dimensions;i++){
         let square = document.createElement('div');
-        square.classList.add('square');
-        square.setAttribute('style','height: 16px; width: 16px;background: pink;');
+        square.setAttribute('class','squares');
+        square.setAttribute('style','height: 16px; width: 16px;');
         grid.appendChild(square);
+        
     }
     container.appendChild(grid);
+    
 }
 
-makeGrid(16);
+const container = document.querySelector('#container');
+makeGrid(16); 
+const squareColor = document.querySelectorAll('.squares');
+
+squareColor.forEach((square) =>{
+    square.addEventListener('mouseover',(e)=>
+    e.target.style.background = 'red');
+});
