13b40ea0cd8db8f3963ec3a0d1081e5984027160
tony <nintego92@gmail.com>
2022-10-12 20:17:51 -0500

    Add gridBtn

--- etch-sketch-script.js
+++ etch-sketch-script.js
@@ -13,15 +13,41 @@ function makeGrid(dimensions){
         grid.appendChild(square);
         
     }
+  
     container.appendChild(grid);
     
 }
 
-const container = document.querySelector('#container');
-makeGrid(16); 
-const squareColor = document.querySelectorAll('.squares');
 
+const container = document.querySelector('#container');
+const gridBtn = document.querySelector('#gridBtn');
+let gridSize = 16;
+
+gridBtn.addEventListener('click',function(){
+    gridSize = prompt("Enter new grid size");
+    const removeGrid = document.querySelector('.grid');
+    
+    if(gridSize>100){
+        alert("That grid size is too big!");
+        return;
+    }
+    
+    else{  
+        removeGrid.remove(); 
+        makeGrid(gridSize);
+    }
+    const squareColor = document.querySelectorAll('.squares');
+    squareColor.forEach((square) =>{
+    square.addEventListener('mouseover',(e)=>
+    e.target.style.background = 'red');
+});
+}) ; 
+
+makeGrid(gridSize);
+
+const squareColor = document.querySelectorAll('.squares');
 squareColor.forEach((square) =>{
     square.addEventListener('mouseover',(e)=>
     e.target.style.background = 'red');
 });
+
