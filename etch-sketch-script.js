932732c4916cd8cb39db531d2219fc429790fc7c
tony <nintego92@gmail.com>
2022-10-14 19:40:39 -0500

    Add randomColor feature

--- etch-sketch-script.js
+++ etch-sketch-script.js
@@ -2,7 +2,8 @@ function makeGrid(dimensions = 16){
 
     const grid = document.createElement('div');
     grid.classList.add('grid');
-    grid.style =`display: grid; border: solid black; height: 960px; width: 960px ;grid-template-columns: repeat(${dimensions},1fr);`;
+    grid.style =`display: grid; border: solid black; height: 960px; width: 960px 
+    ;grid-template-columns: repeat(${dimensions},1fr);`;
 
     for( let i = 0 ; i<dimensions ** 2 ; i++ ){
 
@@ -18,7 +19,9 @@ function makeGrid(dimensions = 16){
     squareColor.forEach((square) =>{
 
         square.addEventListener('mouseover',(e)=>
-        e.target.style.background = 'red');
+        e.target.style.background = `rgb(${Math.floor(Math.random()*256)},
+        ${Math.floor(Math.random()*256)},
+        ${Math.floor(Math.random()*256)})`);
 
     });
 
@@ -28,10 +31,10 @@ const container = document.querySelector('#container');
 const gridBtn = document.querySelector('#gridBtn');
 
 gridBtn.addEventListener('click',function(){
-    const gridSize = Number(prompt("Enter new grid size"));
+    const gridSize = Number(prompt("Enter new grid size")||16);
     const removeGrid = document.querySelector('.grid');
     
-    if(gridSize>100 || gridSize <0 || gridSize === null){
+    if(gridSize>100 || gridSize <0){
         alert("Enter a number higher than 0 or lower than 100!");
         return;
     }
@@ -41,12 +44,6 @@ gridBtn.addEventListener('click',function(){
         makeGrid(gridSize);
     }
 
-    /*const squareColor = document.querySelectorAll('.squares');
-    squareColor.forEach((square) =>{
-        square.addEventListener('mouseover',(e)=>
-        e.target.style.background = 'red');
-
-    });*/
 }); 
 
 makeGrid();
